dba2ed5da81232c2c4fa9e78fd6a1b732a82123d
Dean <deaneffi1414@gmail.com>
2024-06-22 18:59:28 +0300

    fix is.length excutions to use min-max

--- src/validators.ts
+++ src/validators.ts
@@ -2,40 +2,60 @@ import { body } from "express-validator";
 const titleValidation = body("title")
   .isString()
   .trim()
-  .isLength(2)
-  .withMessage("title must have at least 2 characters.");
-const contentValidation = body("content")
+  .isLength({
+    min: 2,
+    max: 25,
+  })
+  .withMessage("title be between 2 and 25 characters.");
+const postContentValidation = body("content")
   .isString()
   .trim()
-  .isLength(2)
+  .isLength({
+    min: 2,
+  })
   .withMessage("content must have at least 2 characters.");
+
+const commentContentValidation = body("content")
+  .isString()
+  .trim()
+  .isLength({
+    min: 2,
+    max: 100,
+  })
+  .withMessage("content must have at between 2 and 100 characters.");
 
 const userValidation = body("username")
   .isString()
   .trim()
-  .isLength(2)
-  .withMessage("username must have at least 2 characters.");
+  .isLength({
+    min: 2,
+    max: 20,
+  })
+  .withMessage("username must have between 2 and 20 characters.");
 
 const passwordValidation = body("password")
   .isString()
   .trim()
-  .isLength(3)
-  .withMessage("password must have at least 3 characters.");
+  .isLength({
+    min: 2,
+    max: 25,
+  })
+  .withMessage("password must have between 2 and 25 characters.");
 
 const isPublicValidation = body("isPublic")
   .isBoolean()
-  .withMessage("isPublic must be a Boolean");
+  .withMessage("isPublic must be a Boolean.");
 
 //validate comment model
 export const commentValidation = [
   userValidation,
   titleValidation,
-  contentValidation,
+  commentContentValidation,
 ];
 export const postValidation = [
   isPublicValidation,
   titleValidation,
-  contentValidation,
+  postContentValidation,
 ];
 
 export const loginValidation = [userValidation, passwordValidation];
